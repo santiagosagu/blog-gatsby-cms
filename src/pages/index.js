3a5db7b0038b62191202e0a4245189342ecae79f
Danielgiraldo <daniel.giraldo@atlantisoft.us>
2022-07-12 21:44:35 -0500

    correction script

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,16 +29,6 @@ const BlogIndex = ({ data, location }) => {
       <Seo title="All posts" />
       <Helmet>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        <script>
-          if (window.netlifyIdentity){" "}
-          {window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/"
-              })
-            }
-          })}
-        </script>
       </Helmet>
       <Bio />
       <ol style={{ listStyle: `none` }}>
